313559018c6c63455da5be2d968fc16d44bafeba
Abu Dawud <warishafidz@gmail.com>
2019-09-24 19:45:49 +0700

    Whitelabel Metadata (siteMetadata) - change siteMetadata: title, description, and author - add jemberDev icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Tailwind`,
-    description: `Gatsby starter styled with Tailwind`,
-    author: `@taylorbryant`
+    title: `JemberDev`,
+    description: `JemberDev merupakan sebuah komunitas yang menjadi wadah bagi semua penggiat IT untuk menuangkan ide-ide kreatif guna menghasilkan teknologi yang dapat bermanfaat bagi masyarakat Indonesia khususnya daerah Jember ( :`,
+    author: `https://github.com/jember-dev`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -15,7 +15,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#4dc0b5`,
         display: `minimal-ui`,
-        icon: `src/images/tailwind-icon.png`
+        icon: `src/images/jember.dev-icon.jpg`
       }
     },
     `gatsby-plugin-postcss`,
